3ea64b42c016b63bb3f6f63104b10074f446910b
paito0909 <paito.naranjoq@gmail.com>
2024-03-18 13:12:32 -0500

    Parte 3

--- main.py
+++ main.py
@@ -22,3 +22,28 @@ while True:
         print("↓")
     else:
         print("Movimiento inválido")
+        
+        
+#Para esta sección del proyecto integrador necesitaremos aprender a manipular la terminal:
+
+#Iniciar con un número en 0, leer la tecla `n` del teclado en un bucle, por cada presionada, borrar la terminal y e imprimir el nuevo número hasta el número 50.
+
+#La operación de borrar la terminal e imprimir el nuevo número debe estar en su propia función.
+
+#Para borrar la terminal antes de imprimir nuevo contenido usar la instrucción: os.system('cls' if os.name=='nt' else 'clear'), para esto se debe importar la librería os
+
+import os
+
+def clear_terminal():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def main():
+    numero = 0
+    while numero <= 50:
+        input("Presiona 'n' para continuar...")
+        clear_terminal()
+        print(numero)
+        numero += 1
+
+if __name__ == "__main__":
+    main()
