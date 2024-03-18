b85904a9d772a9048bcf6bfd36dbff365bb4affa
paito0909 <paito.naranjoq@gmail.com>
2024-03-18 15:10:00 -0500

    Revert "Parte 4 final"
    This reverts commit 2950af9d28ecd3ae49687724f5c0a484a0d6016b.

--- laberinto.py
+++ laberinto.py
@@ -32,51 +32,10 @@
 #Si la nueva posición es válida, actualizar (px, py), poner el caracter P en esta nueva coordenada y restaurar la anterior a mostrar
 
 import os
-import readchar
+import tkinter as tk
 
-def clear_terminal():
-    #Función para limpiar la terminal""
-    os.system('cls' if os.name=='nt' else 'clear')
-
-def convertir_mapa_a_matriz(laberinto):
-    #Función para convertir el mapa de laberinto en una matriz de caracteres
-    return [list(fila) for fila in laberinto.split("\n")]
-
-def mostrar_mapa(mapa):
-    #Función para mostrar el mapa en la terminal
-    clear_terminal()
-    for fila in mapa:
-        print("".join(fila))
-
-def main_loop(mapa, posicion_inicial, posicion_final):
-    #Función para el bucle principal del juego
-    px, py = posicion_inicial
-    
-    while (px, py) != posicion_final:
-        mapa[py][px] = 'P'  
-        mostrar_mapa(mapa)
-
-        tecla = readchar.readkey()
-        clear_terminal()
-        
-        if tecla == '\x1b[A' and py > 0 and mapa[py - 1][px] != '#':
-            mapa[py][px] = '.'  
-            py -= 1
-        elif tecla == '\x1b[B' and py < len(mapa) - 1 and mapa[py + 1][px] != '#':
-            mapa[py][px] = '.'  
-            py += 1
-        elif tecla == '\x1b[C' and px < len(mapa[0]) - 1 and mapa[py][px + 1] != '#':
-            mapa[py][px] = '.' 
-            px += 1
-        elif tecla == '\x1b[D' and px > 0 and mapa[py][px - 1] != '#':
-            mapa[py][px] = '.' 
-            px -= 1
-
-    mostrar_mapa(mapa)
-    print("¡Felicidades! Has llegado al final del laberinto.")
-
-if __name__ == "__main__":
-    laberinto = """..###################
+# Define el laberinto
+laberinto = '''..###################
 ....#...............#
 #.#.#####.#########.#
 #.#...........#.#.#.#
@@ -96,8 +55,86 @@ if __name__ == "__main__":
 #.#.#.#.#.#...#.#...#
 #.#.#.#.#.#.#.#.#.#.#
 #.....#.....#.#.#.#.#
-###################.."""
-    mapa = convertir_mapa_a_matriz(laberinto)
-    posicion_inicial = (0, 0)
-    posicion_final = (len(mapa[0]) - 1, len(mapa) - 1)
-    main_loop(mapa, posicion_inicial, posicion_final)
+###################..'''
+
+def convertir_mapa_a_matriz(laberinto):
+    """Función para convertir el mapa de laberinto en una matriz de caracteres"""
+    return [list(fila) for fila in laberinto.split("\n")]
+
+def mostrar_mapa():
+    """Función para mostrar el mapa en la ventana"""
+    canvas.delete("all")
+    for y, fila in enumerate(mapa):
+        for x, celda in enumerate(fila):
+            if celda == '#':
+                canvas.create_rectangle(x*20, y*20, (x+1)*20, (y+1)*20, fill="black")
+            elif celda == 'P':
+                canvas.create_rectangle(x*20, y*20, (x+1)*20, (y+1)*20, fill="red")
+    ventana.update()
+
+def mover_arriba(event):
+    """Función para mover hacia arriba"""
+    global py
+    if py > 0 and mapa[py - 1][px] != '#':
+        mapa[py][px] = '.'  # Restaurar espacio vacío
+        py -= 1
+        mapa[py][px] = 'P'  # Actualizar posición del jugador
+        mostrar_mapa()
+
+def mover_abajo(event):
+    """Función para mover hacia abajo"""
+    global py
+    if py < len(mapa) - 1 and mapa[py + 1][px] != '#':
+        mapa[py][px] = '.'  # Restaurar espacio vacío
+        py += 1
+        mapa[py][px] = 'P'  # Actualizar posición del jugador
+        mostrar_mapa()
+
+def mover_izquierda(event):
+    """Función para mover hacia la izquierda"""
+    global px
+    if px > 0 and mapa[py][px - 1] != '#':
+        mapa[py][px] = '.'  # Restaurar espacio vacío
+        px -= 1
+        mapa[py][px] = 'P'  # Actualizar posición del jugador
+        mostrar_mapa()
+
+def mover_derecha(event):
+    """Función para mover hacia la derecha"""
+    global px
+    if px < len(mapa[0]) - 1 and mapa[py][px + 1] != '#':
+        mapa[py][px] = '.'  # Restaurar espacio vacío
+        px += 1
+        mapa[py][px] = 'P'  # Actualizar posición del jugador
+        mostrar_mapa()
+
+# Configurar la ventana
+ventana = tk.Tk()
+ventana.title("Laberinto")
+
+# Configurar el lienzo
+canvas = tk.Canvas(ventana, width=20*len(laberinto.split('\n')[0]), height=20*len(laberinto.split('\n')))
+canvas.pack()
+
+# Convertir el laberinto a matriz
+mapa = convertir_mapa_a_matriz(laberinto)
+
+# Encontrar la posición inicial del jugador
+py, px = 0, 0
+for y, fila in enumerate(mapa):
+    for x, celda in enumerate(fila):
+        if celda == 'P':
+            py, px = y, x
+            break
+
+# Mostrar el laberinto inicialmente
+mostrar_mapa()
+
+# Configurar los eventos de teclado
+ventana.bind("<Up>", mover_arriba)
+ventana.bind("<Down>", mover_abajo)
+ventana.bind("<Left>", mover_izquierda)
+ventana.bind("<Right>", mover_derecha)
+
+# Iniciar el bucle de la ventana
+ventana.mainloop()
